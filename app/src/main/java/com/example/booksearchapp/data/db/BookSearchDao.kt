e6941a107791def6a46017bd83dcf3a18d3264d7
DESKTOP-4LFJF15\rnjss <rnjsskfk123@naver.com>
2022-09-25 23:37:28 +0900

    jetpack Room을 사용하여 검색결과 저장을 위한 DB 구현

---
+++ app/src/main/java/com/example/booksearchapp/data/db/BookSearchDao.kt
@@ -0,0 +1,19 @@
+package com.example.booksearchapp.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.booksearchapp.data.model.Book
+
+@Dao
+interface BookSearchDao {
+
+    // Query를 제외한 CUD는 시간이 걸리기 때문에 코루틴 안에서 비동기로 수행할 예정으로 suspend로 함수 구현
+    @Insert(onConflict = OnConflictStrategy.REPLACE) // 동일한 isbn을 가진 데이터가 존재할 경우 덮어씌우기
+    suspend fun insertBook(book: Book)
+
+    @Delete
+    suspend fun deleteBook(book: Book)
+
+    @Query("SELECT * FROM books")
+    fun getFavoriteBooks(): LiveData<List<Book>>
+}
