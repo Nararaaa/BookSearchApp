e8916e6b605186ab732f9f38e75e129cf4e182c1
DESKTOP-4LFJF15\rnjss <rnjsskfk123@naver.com>
2022-09-26 16:00:34 +0900

    jetpack Room Database 응답에 Paging 적용하기

--- app/src/main/java/com/example/booksearchapp/data/repository/BookSearchRepositoryImpl.kt
+++ app/src/main/java/com/example/booksearchapp/data/repository/BookSearchRepositoryImpl.kt
@@ -5,11 +5,15 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.example.booksearchapp.data.api.RetrofitInstance.api
 import com.example.booksearchapp.data.db.BookSearchDatabase
 import com.example.booksearchapp.data.model.Book
 import com.example.booksearchapp.data.model.SearchResponse
 import com.example.booksearchapp.data.repository.BookSearchRepositoryImpl.PreferencesKeys.SORT_MODE
+import com.example.booksearchapp.util.Constants.PAGING_SIZE
 import com.example.booksearchapp.util.Sort
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
@@ -67,4 +71,21 @@ class BookSearchRepositoryImpl(
                 prefs[SORT_MODE] ?: Sort.ACCURACY.value
             }
     }
+
+    // Paging
+    override fun getFavoritePagingBooks(): Flow<PagingData<Book>> {
+        val pagingSourceFactory = { db.bookSearchDao().getFavoritePagingBooks() }
+
+        return Pager(
+            //PagingConfig를 통해 파마미터 전달
+            config = PagingConfig(
+                pageSize = PAGING_SIZE,
+                enablePlaceholders = false,
+                maxSize = PAGING_SIZE * 3
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
+    }
+
+
 }
--- app/src/main/java/com/example/booksearchapp/ui/view/FavoriteFragment.kt
+++ app/src/main/java/com/example/booksearchapp/ui/view/FavoriteFragment.kt
@@ -11,7 +11,7 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.booksearchapp.databinding.FragmentFavoriteBinding
-import com.example.booksearchapp.ui.adapter.BookSearchAdapter
+import com.example.booksearchapp.ui.adapter.BookSearchPagingAdapter
 import com.example.booksearchapp.ui.viewmodel.BookSearchViewModel
 import com.example.booksearchapp.util.collectLatestStateFlow
 import com.google.android.material.snackbar.Snackbar
@@ -21,7 +21,9 @@ class FavoriteFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var bookSearchViewModel: BookSearchViewModel
-    private lateinit var bookSearchAdapter: BookSearchAdapter
+
+    //private lateinit var bookSearchAdapter: BookSearchAdapter
+    private lateinit var bookSearchAdapter: BookSearchPagingAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -56,15 +58,20 @@ class FavoriteFragment : Fragment() {
                 }
             }
         }*/
-        // 코루틴 확장함수 사용
+        /*// 코루틴 확장함수 사용
         collectLatestStateFlow(bookSearchViewModel.favoriteBooks) {
             bookSearchAdapter.submitList(it)
+        }*/
+        /*collectLatestStateFlow((bookSearchViewModel.favoritePagingBooks))*/
+        collectLatestStateFlow(bookSearchViewModel.favoritePagingBooks) {
+            bookSearchAdapter.submitData(it)
         }
     }
 
 
     private fun setupRecyclerView() {
-        bookSearchAdapter = BookSearchAdapter()
+        //bookSearchAdapter = BookSearchAdapter()
+        bookSearchAdapter = BookSearchPagingAdapter()
         binding.rvFavoriteBooks.apply {
             setHasFixedSize(true)
             layoutManager =
@@ -98,13 +105,24 @@ class FavoriteFragment : Fragment() {
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 val position = viewHolder.bindingAdapterPosition
+                /*
                 val book = bookSearchAdapter.currentList[position]
                 bookSearchViewModel.deleteBook(book)
                 Snackbar.make(view, "Book has deleted", Snackbar.LENGTH_SHORT).apply {
                     setAction("Undo") {
                         bookSearchViewModel.saveBook(book)
                     }
-                }.show()
+                }.show()*/
+
+                val pageBook = bookSearchAdapter.peek(position)
+                pageBook?.let { book ->
+                    bookSearchViewModel.deleteBook(book)
+                    Snackbar.make(view, "Book has deleted", Snackbar.LENGTH_SHORT).apply {
+                        setAction("Undo") {
+                            bookSearchViewModel.saveBook(book)
+                        }
+                    }.show()
+                }
             }
         }
         ItemTouchHelper(itemTouchHelperCallback).apply {
